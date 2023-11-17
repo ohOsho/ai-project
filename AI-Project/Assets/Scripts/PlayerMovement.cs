f585e87efc93e4dd3a0c0997260febea4942f91d
scolety1 <smcolety@gmail.com>
2023-11-17 11:37:08 -0700

    Movement Script

--- AI-Project/Assets/Scripts/PlayerMovement.cs
+++ AI-Project/Assets/Scripts/PlayerMovement.cs
@@ -6,13 +6,19 @@ using UnityEngine.AI;
 public class PlayerMovement: MonoBehaviour
 {
     public NavMeshAgent mAgent;
+    public Rigidbody rb;
 
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Vector3 mouse = Input.mousePosition;
+            Vector3 stop = new Vector3(rb.velocity.x, 0, 0);
 
             Ray castPoint = Camera.main.ScreenPointToRay(mouse);
 
